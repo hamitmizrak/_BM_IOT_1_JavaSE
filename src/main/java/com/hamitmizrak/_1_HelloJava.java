88dd53111147a5c298f95aa53bccc0533f14941c
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-14 17:35:10 +0300

    sout

--- src/main/java/com/hamitmizrak/_1_HelloJava.java
+++ src/main/java/com/hamitmizrak/_1_HelloJava.java
@@ -9,6 +9,6 @@ public class _1_HelloJava {
     // psvm
     public static void main(String[] args) {
         // sout
-        System.out.println("Merhabalar Güzel İnsanlar, Nasılsınız.");
+        System.out.println("üğşçö Merhabalar Güzel İnsanlar, Nasılsınız.");
     } //end psvm
 } // end class
