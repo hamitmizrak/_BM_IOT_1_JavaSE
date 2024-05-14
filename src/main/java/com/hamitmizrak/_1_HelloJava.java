c0c983c40fd2c4a2501f42c975118de674fb9650
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-14 16:37:43 +0300

    Hello Java

--- src/main/java/com/hamitmizrak/_1_HelloJava.java
+++ src/main/java/com/hamitmizrak/_1_HelloJava.java
@@ -1,2 +1,14 @@
-package com.hamitmizrak;public class _1_HelloJava {
-}
+package com.hamitmizrak;
+
+// Yorumlama (Single Comment)
+/*
+Çoklu satırlar
+*/
+public class _1_HelloJava {
+
+    // psvm
+    public static void main(String[] args) {
+
+    } //end psvm
+
+} // end class
