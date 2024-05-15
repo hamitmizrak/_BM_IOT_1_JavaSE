13e980d08254b6f72ff2b59f7b268a8c1e8f61cb
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-15 17:22:45 +0300

    Homework1

---
+++ src/main/java/com/hamitmizrak/Homework1.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+public class Homework1 {
+    // Ödev: Yukarıdaki öreneğe benzer olarak
+        /*
+        number1=18
+        number2=6
+        bu sayılarda (+ - * / % ) işlemleri consolda yazdıralım ?
+        * */
+    public static void main(String[] args) {
+
+    }
+}
