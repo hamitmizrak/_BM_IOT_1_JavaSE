5cdc684e632f5271329ad5f75c59ff76b0949331
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-15 15:55:57 +0300

    Primitive Types

---
+++ src/main/java/com/hamitmizrak/_3_PrimitiveTypes.java
@@ -0,0 +1,28 @@
+package com.hamitmizrak;
+
+public class _3_PrimitiveTypes {
+
+    public static void main(String[] args) {
+        System.out.println("Primitive Types");
+        // Data Structure
+
+        // 8 tane primitive type vardır.
+        // Java neden 100% OOP değildir ? cevap: primitive türden dolayı
+
+        // Sayılar için (4)
+        byte b1=127;  // 1 byte=8 bit -128<=SAYI<=+127
+        short s1=128; // 16 bit
+        int i1=12812155;
+        long l1=125212545555L;
+
+        // Virgüllü Sayılar (Floating Point) (2)
+        float f2=14.53f;
+        double d2=14525.5445;
+
+        // boolean(1)
+        boolean b3=true; //false
+
+        // char (1)
+        char c5='a';
+    }
+}
