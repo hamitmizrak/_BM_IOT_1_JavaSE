d45d63513dd0075f9312810435263933ac32e06d
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-15 15:40:17 +0300

    VariableName

--- src/main/java/com/hamitmizrak/_2_Variable.java
+++ src/main/java/com/hamitmizrak/_2_Variable.java
@@ -2,20 +2,27 @@ package com.hamitmizrak;
 
 public class _2_Variable {
 
+    // psvm => TAB
     public static void main(String[] args) {
         System.out.println("Variable");
+        // Shortcut Beautify : Ctrl+Alt+l
 
         // Değişken yazarken kullanacağım kurallar
         // camelCase kuralına göre yazıyoruz.
         // isim kullanın,sıfat
 
-        String variableName="11";
+        String variableName = "11";
         System.out.println(variableName);
 
         // Değişken adlarında yazabileceğim kurallar
-        String _variableName="22";
-        String $variableName="33";
-        String variableName89="44";
+        String _variableName = "22";
+        System.out.println(_variableName);
+
+        String $variableName = "33";
+        System.out.println($variableName);
+
+        String variableName89 = "44";
+        System.out.println(variableName89);
 
         // Değişken adlarında yazamazsınız kuralla
         //String 44variableName="44";
