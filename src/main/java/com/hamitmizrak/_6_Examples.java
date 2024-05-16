10c37884df9c5f80d911d049874af2e04052b7ca
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-16 15:34:43 +0300

    Examples-1

--- src/main/java/com/hamitmizrak/_6_Examples.java
+++ src/main/java/com/hamitmizrak/_6_Examples.java
@@ -2,10 +2,20 @@ package com.hamitmizrak;
 
 public class _6_Examples {
     /*
-    1.dereceden 2 bilinmeyelin denklem
+    1.dereceden 2 bilinmeyenli denklem
     z=4x+5y
     x=3;
-    y=6 verelim buradaki değerlere göre z'yi bulan Java Algoritmasını yazınız
+    y=6 değerlerini verelim.
+    Buradaki değerlere göre z'yi bulan Java Algoritmasını yazınız ?
     Çözüm:42 bulmamız gerekiyor
      */
+
+    // psvm
+    public static void main(String[] args) {
+        int x, y, z;
+        x=3;
+        y=6;
+        z=4*x+5*y;
+        System.out.println("Cevap: "+z);
+    }
 }
