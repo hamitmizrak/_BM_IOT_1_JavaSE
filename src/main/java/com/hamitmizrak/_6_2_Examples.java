91ba047be9d40ca7d52f9c73f76ea50caeac78d7
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-16 15:57:17 +0300

    Examples-2

--- src/main/java/com/hamitmizrak/_6_2_Examples.java
+++ src/main/java/com/hamitmizrak/_6_2_Examples.java
@@ -7,4 +7,11 @@ public class _6_2_Examples {
     0 derece kaç fahranhattır ? Cevap: 32F
     */
 
+    public static void main(String[] args) {
+        double celcius, fahranhat;
+        celcius=0;
+        fahranhat=celcius*9/5+32;
+        System.out.println(celcius+" derece "+fahranhat+" Fahranhayttır.");
+    }
+
 }
