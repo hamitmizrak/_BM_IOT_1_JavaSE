263c51a56fa71c4e23000a9feda14116a30de532
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-17 16:33:04 +0300

    Math

---
+++ src/main/java/com/hamitmizrak/_8_1_Math.java
@@ -0,0 +1,15 @@
+package com.hamitmizrak;
+
+import java.util.Scanner;
+
+public class _8_1_Math {
+
+    public static void main(String[] args) {
+
+        // Mathematics (Math)
+        System.out.println("PI sayısı: "+Math.PI);
+        System.out.println("Mutlak Değer:"+ Math.abs(-25));
+        System.out.println("Karekök Değer:"+ Math.sqrt(36));
+        System.out.println("Üslü Değer:"+ Math.pow(2,5)); //2 üzerine 5
+    }
+}
